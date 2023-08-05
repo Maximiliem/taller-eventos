2f7bcd6dcbbdd05d7400840940e018d8c8fc2778
Maximiliem <maximiliem@yahoo.com>
2023-08-05 12:04:42 -0300

    Agregado de if para evitar propagacion del evento generado desde DOM

--- eventos.js
+++ eventos.js
@@ -1,6 +1,9 @@
 const div1 = document.querySelector("div");
 
-function saludoDiv(){
+function saludoDiv(event){
+    if(event.target.tagName !=="BUTTON")
+    /*Se utiliza event.target para acceder al elemento que dispara el evento, en este caso, el elemento sobre el que se hace clic. Con la propiedad tagName del elemento event.target se obtiene el nombre de la etiqueta HTML del elemento que dispara el evento.
+    La condición if verifica si el elemento que dispara el evento no es un botón */
     alert("Hola! Soy el div");
 }
 
